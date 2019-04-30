fe031975f224d0c4a1eabd764912adc21cdda62b
Kate <you@example.com>
2019-04-29 18:25:46 -0700

    axios, spotify, and adding all the things that are needed

---
+++ liri-node.js
@@ -0,0 +1,37 @@
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+ 
+// Optionally the request above could also be done as
+axios.get('/user', {
+    params: {
+      ID: 12345
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id:`905347c6f9c14c6e8028d655f4c2f2f5`,
+  secret: `cbfc6a5a09ab474a9daf6046a7db0bdc`
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
