ce1d63489115cbd0d52d1550e646ed9e20d6aa18
Kate <you@example.com>
2019-05-01 18:12:04 -0700

    changes and adding files that were needed

--- liri-node.js
+++ liri-node.js
@@ -1,22 +1,35 @@
-//  require('dotenv').config()
+ require('dotenv').config()
 
-// const result = dotenv.config()
+const result = dotenv.config()
  
-// if (result.error) {
-//   throw result.error
-// }
+if (result.error) {
+  throw result.error
+}
  
-// console.log(result.parsed)
+console.log(result.parsed)
 
-// Make a request for a user with a given ID
+var keys = require("./keys.js");
+
 var axios = require("axios");
+  concert.this = function() {
 
-axios.get('/user?ID=12345')
+  }
+
+  //http://app.swaggerhub.com/apis/Bandsintown/PublicAPI/3.0.0
+
+  //id: f1e2a355101ef09d3565d7053ba70f0
+//capture user input (think process.argv3)
+//"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+axios.get({
+  method:'get',
+  url:'http://bit.ly/2mTM3nY',
+  responseType:'json'
+})
   .then(function (response) {
-    console.log(response);
+    // console.log(response);
   })
   .catch(function (error) {
-    console.log(error);
+    // console.log(error);
   });
  
    var moment = require('moment');
@@ -24,15 +37,15 @@ axios.get('/user?ID=12345')
 
 
 var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id:`905347c6f9c14c6e8028d655f4c2f2f5`,
-  secret: `cbfc6a5a09ab474a9daf6046a7db0bdc`
-});
+var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify({
+//   id:`905347c6f9c14c6e8028d655f4c2f2f5`,
+//   secret: `cbfc6a5a09ab474a9daf6046a7db0bdc`
+// });
  
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
   if (err) {
-    return console.log('Error occurred: ' + err);
+    // return console.log('Error occurred: ' + err);
   }
  
 console.log(data); 
